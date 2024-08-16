adfc42ea863bc2a601da7d6889430d36e25ee9b0
Snehatk2002 <sammu5285@gmail.com>
2024-08-16 19:55:14 +0530

    created

---
+++ models/doctor.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+const doctorSchema=mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        qualification:{type:String,required:true},
+        specialisation:{type:String,required:true},
+        contact:{type:String,required:true}
+
+
+    }
+)
+const doctorModel =mongoose.model("doctors",doctorSchema)
+module.exports=doctorModel
